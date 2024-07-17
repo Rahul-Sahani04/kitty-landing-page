7b2b13d4478a96c5a42dfee200702ad8d756d31e
Rahul <110347707+Rahul-Sahani04@users.noreply.github.com>
2024-07-18 00:18:10 +0530

    chore: Update homepage URL in package.json and App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const ErrorPage = () => {
             home page.{" "}
           </p>
           <a
-            href="#"
+            href="/home"
             class="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
           >
             Back to Homepage
@@ -34,11 +34,11 @@ const ErrorPage = () => {
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/home",
     element: <Home />,
   },
 
-  // 404 page
+  // 404 page if no route is found
   {
     path: "*",
     element: <ErrorPage />,
